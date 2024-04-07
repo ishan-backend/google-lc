503e589194d3da1167afa81d03b4a73df915cf1f
kakkarot <88132188+ishan-backend@users.noreply.github.com>
2024-04-07 18:42:01 +0530

    Update little_elephants_and_tshirts.cpp

--- dp/bitmasking/little_elephants_and_tshirts.cpp
+++ dp/bitmasking/little_elephants_and_tshirts.cpp
@@ -15,6 +15,14 @@ Ans = 4 ways i.e. (3, 8), (3, 50), (5, 8), (5, 50)
 
 dp(i, mask): no of ways for person from Pi till Pn to wear tshirts to a party such that Pi till Pn only wear tshirts mentioned by mask
 Mask will have 100 bits. jth bit of this mask will tell if it is available to wear if set, if unset that means it has been wore by person from P1 to Pi-1.
+
+that will be dp(1, (1111..100 times)base2) and 2^100 is very large hence the SC is very bad.
+Let's try to optimize further
+
+So let's reverse the dp state
+Tshirts <= 100 count
+Person <= 10 count
+dp(i, mask): ways to assign  to ith t-shirt to mask i.e. people left to wear/be assigned a t-shirt.
 */
 
 
