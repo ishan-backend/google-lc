c34a4adfccdad487891f3e9e84334e801d2981cc
kakkarot <88132188+ishan-backend@users.noreply.github.com>
2024-04-07 17:51:23 +0530

    Create travelling_salesman.cpp

---
+++ dp/bitmasking/travelling_salesman.cpp
@@ -0,0 +1,12 @@
+/*
+NP Hard Problem: (With bitmasking we will be able to relax the TC by some measure)
+
+Given cities and distances between each pair of cities (two-way costs).
+Shortest possible route that visits each city and returns to origin city.
+
+Starts from city 1.
+Visit each city from 2..N exactly once, and then he'll be coming back home i.e. city 1
+
+Design a tour, such that overall cost is minimised.
+*/
+
