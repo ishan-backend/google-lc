51bda14e3626212f883b68e3a1357a62adbc9ec2
kakkarot <88132188+ishan-backend@users.noreply.github.com>
2024-04-07 18:31:10 +0530

    Create little_elephants_and_tshirts.cpp

---
+++ dp/bitmasking/little_elephants_and_tshirts.cpp
@@ -0,0 +1,16 @@
+/*
+https://www.codechef.com/problems/TSHIRTS
+
+Kinds of tshirts: T1, T2 .... T100
+N persons. Each person can own some of these tshirts. Every person has max one tshirt of an ID.
+Multiple persons can have same tshirt ids.
+
+No of ways - in which people can wear tshirts they own. Such that no two person in the party are wearing tshirt with same ID.
+
+N = 2
+p1 = 3 5
+p2 = 8 50
+
+Ans = 4 ways i.e. (3, 8), (3, 50), (5, 8), (5, 50)
+*/
+
