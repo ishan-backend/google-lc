388cc6653cab8c8405bf571008c8924dcf96001e
kakkarot <88132188+ishan-backend@users.noreply.github.com>
2024-04-07 13:58:45 +0530

    Update subarray_sum.cpp

--- segment-tree/subarray_sum.cpp
+++ segment-tree/subarray_sum.cpp
@@ -19,7 +19,23 @@ void build(int node, int st, int en) { // node 1 of st ;st, en is [0, n-1] index
     st[node] = st[2*node] + st[2*node+1]; // subarray sum program
 }
 
+// cases: complete and partial overlap
+int query(int node, int st, int en, int l, int r) { // [l, r]  are of query and [st, en] is what is being taken care of by node
+    if(st > r or en < l) {
+        return 0; // subarray sum response
+    }
 
+    // l st en r
+    if(l <= st and r >= en) {
+        return st[node]; // full overlap response for subarray sum
+    }
+
+    // st l en r || l st r en
+    int mid = (st+en)/2;
+    int q1 = query(2*node, st, mid, l, r);
+    int q2 = query(2*node+1, mid+1, en, l, r);
+    return q1+q2; // subarray sum
+}
 
 
 int main()
@@ -34,6 +50,19 @@ int main()
     build(1, 0, n-1);
     for(int i=0; i<n; i++) {
         cout << st[i] << endl;
+    }
+
+    int q;
+    cin>>q;
+    while(q--){
+        int type;
+        cin >> type;
+        if(type == 1) {
+            int l, r;
+            cin>>l>>r;
+            int ans = query(1, 0, n-1, l, r);
+            cout << ans << endl;
+        }
     }
     return 0;
 }
