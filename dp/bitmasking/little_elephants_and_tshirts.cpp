dcebf26abaa97407d8758cdbd205584e54e6902c
kakkarot <88132188+ishan-backend@users.noreply.github.com>
2024-04-07 18:35:26 +0530

    Update little_elephants_and_tshirts.cpp

--- dp/bitmasking/little_elephants_and_tshirts.cpp
+++ dp/bitmasking/little_elephants_and_tshirts.cpp
@@ -12,5 +12,10 @@ p1 = 3 5
 p2 = 8 50
 
 Ans = 4 ways i.e. (3, 8), (3, 50), (5, 8), (5, 50)
+
+dp(i, mask): no of ways for person from Pi till Pn to wear tshirts to a party such that Pi till Pn only wear tshirts mentioned by mask
+Mask will have 100 bits. jth bit of this mask will tell if it is available to wear if set, if unset that means it has been wore by person from P1 to Pi-1.
 */
+
+
 
