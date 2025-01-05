05006135e8d781cedc29313f12110f1361de0d72
kakkarot <88132188+ishan-backend@users.noreply.github.com>
2025-01-05 12:04:45 +0530

    Time: 4 ms (46.77%) | Memory: 23.4 MB (36.80%) - LeetSync

---
+++ 155-min-stack/min-stack.cpp
@@ -0,0 +1,20 @@
+class MinStack {
+private:
+    stack<int> s1; //elements in the order of entered
+    stack<int> s2; // minimum so far
+public:
+    void push(int x) {
+	    s1.push(x);
+	    if (s2.empty() || x <= getMin())  s2.push(x);	    
+    }
+    void pop() {
+	    if (s1.top() == getMin())  s2.pop();
+	    s1.pop();
+    }
+    int top() {
+	    return s1.top();
+    }
+    int getMin() {
+	    return s2.top();
+    }
+};
