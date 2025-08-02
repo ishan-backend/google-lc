3dd8d9afde0cb04a8ea9cb749f99b8fed231882a
kakkarot <88132188+ishan-backend@users.noreply.github.com>
2025-08-02 19:16:58 +0530

    Time: 49 ms (23.09%) | Memory: 19.1 MB (81.22%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-
-    int find(vector<int> &par, int x) {
-        while(par[x] != x)
-            x = par[x];
-        return x;
+    int find(vector<int>& par, int x) {
+        if (par[x] != x)
+            par[x] = find(par, par[x]); // path compression is important for this problem for all nodes to finally to one parent node in CC
+        return par[x];
     }
 
-    void merge(vector<int> &par, int i, int j, int &tc) {
+    void merge(vector<int>& par, int i, int j) {
         int p1 = find(par, i);
         int p2 = find(par, j);
-        if(p1 != p2) {
+        if (p1 != p2) {
             par[p1] = p2;
-            tc--;
         }
     }
 
@@ -20,16 +18,31 @@ public:
         // matching rows and columns ko same set me dalo
         int n = stones.size();
         vector<int> par(n);
-        for(int i=0; i<n; i++) par[i] = i;
-        int tc = n;
+        for (int i = 0; i < n; i++)
+            par[i] = i;
+        /*
+            This is a connected components problem in disguise.
+        •	If two stones are in the same row or same column, they’re connected.
+        •	The entire group of connected stones can be reduced to just one stone.
+        •	So if there are k connected stones → you can remove k - 1 stones.
+            
+            The maximum number of stones you can remove =
+    Total stones − Number of connected components
+        */
 
-        for(int i=0; i<n; i++) {
-            for(int j=i+1; j<n; j++) {
-                if(stones[i][0] == stones[j][0] or stones[i][1] == stones[j][1])
-                merge(par, i, j, tc);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (stones[i][0] == stones[j][0] or stones[i][1] == stones[j][1]) {
+                    merge(par, i, j);
+                }
             }
         }
 
-        return n-tc;
+        unordered_set<int> comp;
+        for(int i=0; i<n; i++) {
+            comp.insert(find(par, i));
+        }
+
+        return n - comp.size();
     }
 };
